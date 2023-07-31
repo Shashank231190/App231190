e9a61a43f2e78b446070a150b90e4294e818a183
Shashank Aggarwal <33515190+Shashank231190@users.noreply.github.com>
2023-07-31 16:17:11 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
+from flask import Flask
 from azure.identity import ManagedIdentityCredential
 from azure.keyvault.secrets import SecretClient
-#from azure.identity import DefaultAzureCredential
-from flask import Flask
+
 app = Flask(__name__)
-@app.route("/")
-def MyFun():
-    #return "Hello, World!"
-    keyvault_url = "https://sharesource-vault-1700.vault.azure.net/"
-    credential = ManagedIdentityCredential()
-    secret_client = SecretClient(vault_url=keyvault_url,credential=credential)
-    secret_name = "MyNewSecret"
-    secret = secret_client.get_secret(secret_name)
-    return secret.value
+
+# Initialize the ManagedIdentityCredential
+credential = ManagedIdentityCredential()
+
+# Replace 'YOUR_KEYVAULT_NAME' with the name of your Azure Key Vault
+
+vault_url = f"https://sharesource-vault-1700.vault.azure.net/"
+
+# Initialize the SecretClient with the ManagedIdentityCredential
+client = SecretClient(vault_url=vault_url, credential=credential)
+
+@app.route('/')
+def home():
+    # Replace 'YOUR_SECRET_NAME' with the name of the secret you want to retrieve
+    secret_name = 'MyNewSecret'
+
+    try:
+        # Use the SecretClient to get the secret value
+        secret = client.get_secret(secret_name)
+        return f"The secret value is: {secret.value}"
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+if __name__ == '__main__':
+    app.run()
