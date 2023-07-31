cd809c9c4bef72c7af7010f840584f58f3a2ffdb
Shashank Aggarwal <33515190+Shashank231190@users.noreply.github.com>
2023-07-31 15:58:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,10 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def MyFun():
-    return "Hello, World!"
-    #keyvault_name = os.environ.get("AZURE_VAULT_NAME")
-    #keyvault_url = "https://" +  keyvault_name + ".vault.azure.net/"
-    #credential = DefaultAzureCredential()
-    #secret_client = SecretClient(vault_url=keyvault_url,credential=credential)
-    #secret_name = "MyNewSecret"
-    #secret = secret_client.get_secret(secret_name)
-    #print(f"Secret Value: {secret.value}")
+    #return "Hello, World!"
+    keyvault_url = "https://sharesource-vault-1700.vault.azure.net/"
+    credential = ManagedIdentityCredential()
+    secret_client = SecretClient(vault_url=keyvault_url,credential=credential)
+    secret_name = "MyNewSecret"
+    secret = secret_client.get_secret(secret_name)
+    return secret.value
