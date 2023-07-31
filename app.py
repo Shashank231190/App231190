a3632852b104b3a5a9c2ec7d352abc23a4a1f833
Shashank Aggarwal <33515190+Shashank231190@users.noreply.github.com>
2023-07-31 15:42:31 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+#from azure.identity import ManagedIdentityCredential
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def MyFun():
+    return "Hello, World!"
+    #keyvault_name = os.environ.get("AZURE_VAULT_NAME")
+    #keyvault_url = "https://" +  keyvault_name + ".vault.azure.net/"
+    #credential = DefaultAzureCredential()
+    #secret_client = SecretClient(vault_url=keyvault_url,credential=credential)
+    #secret_name = "MyNewSecret"
+    #secret = secret_client.get_secret(secret_name)
+    #print(f"Secret Value: {secret.value}")
